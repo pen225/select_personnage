35bfc06dddc88a74e5b574f8ae99ea863aeb25aa
assan1997 <49047175+assan1997@users.noreply.github.com>
2021-12-29 16:48:09 +0000

    ybhbhb

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 
 let joueurs = document.querySelectorAll('.joueurs');
-let img = document.querySelectorAll('.img');
+let contain = document.querySelector('.contain');
+let li = document.querySelectorAll('li');
+
 
 let img1 = document.querySelector('.img1');
 let img2 = document.querySelector('.img2');
@@ -11,18 +13,16 @@ let description_joueur = document.querySelector('.description_joueur');
 
 
 joueurs.forEach(element => {
-    console.log(element);
+    // console.log(element);
     element.addEventListener('click', e =>{
-        
         changeJoueur(e);
-
     })
 });
 
 img.forEach(element => {
     
 })
-console.log(img);
+// console.log(img);
 //////// FONCTION
 
 function changeJoueur (e){
@@ -30,41 +30,66 @@ function changeJoueur (e){
         == "joueur1 joueurs") {
             
             console.log(img1.classList);
-             document.querySelector('.active').classList.toggle("disble");
-             document.querySelector('.active').classList.toggle("active");
-             img1.classList.toggle("active")
-             img1.classList.toggle("disble")
+
+            // let ddd = document.querySelector('.active');
+            document.querySelector('.active').classList.toggle("disble");
+            document.querySelector('.active').classList.toggle("active");
+            img1.classList.toggle("active");
+            img1.classList.toggle("disble");
+            const audio = new Audio("../son/rebond1.mp3");
+            audio.play();
+            contain.style.backgroundColor ="#e968f7";
+            description_joueur.style.color= "red";
+            li.style.color="blue"
+            //  description_joueur.textContent = "Lorem ipsum dolor sit";
     }
     else if (e.target.classList.value
         == "joueur2 joueurs") {
         // img2.classList.toggle("active")
         // img1.classList.toggle("disble")
         let ddd = document.querySelector('.active');
-        description_joueur.textContent =`Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Nisi nam exercitationem consequuntur, 
-        sunt quos eveniet fugiat tempora reiciendis sequi iure 
-        unde, nesciunt rerum ea cupiditate. 
-        Reiciendis ea architecto velit mollitia.`
+        
         ddd.classList.toggle("active");
         ddd.classList.toggle("disble");
              img2.classList.toggle("active")
              img2.classList.toggle("disble")
-      
-        console.log(ddd.classList);
-        console.log(img2.classList);
+             const audio = new Audio("../son/SF-rebond2.mp3");
+             audio.play();
+             
+             contain.style.backgroundColor ="#e968f7";
+             description_joueur.style.color= "red";
+             li.style.color="blue"
+        // console.log(ddd.classList);
+        // console.log(img2.classList);
     }else if (e.target.classList.value
         == "joueur3 joueurs") {
             let ddd = document.querySelector('.active')
             ddd.classList.toggle("active");
             ddd.classList.toggle("disble");
-                 img3.classList.toggle("active")
-                 img3.classList.toggle("disble")
+                img3.classList.toggle("active")
+                img3.classList.toggle("disble")
+                const audio = new Audio("../son/SF-hopping.mp3");
+                audio.play();
+                contain.style.backgroundColor ="#e968f7";
+                description_joueur.style.color= "red";
+                li.style.color="blue"
+                 
     }else{
         let ddd = document.querySelector('.active')
         ddd.classList.toggle("active");
         ddd.classList.toggle("disble");
              img4.classList.toggle("active")
              img4.classList.toggle("disble")
+             //Audio
+             const audio = new Audio("../son/toondong.mp3");
+             audio.play();
+             contain.style.backgroundColor ="#e968f7";
+             description_joueur.style.color= "red";
+             li.style.color="blue"
+             //setProperty
+            //  document.documentElement.style.setProperty('--bgColorContain','#2f2c2c');
+            
+            //  description_joueur.style.color.setProperty(--textColor);
     }
 }
     
